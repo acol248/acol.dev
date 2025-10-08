4c9d4a4ce276f44e1e27957bf564e37701819189
acol248 <alex.collyer@acol.dev>
2025-10-08 21:19:02 +0100

    Updated projects items

--- src/app.tsx
+++ src/app.tsx
@@ -288,15 +288,19 @@ export function App() {
               body={
                 <ItemLayout
                   title="Bills Web App"
+                  date="2023 - 2024"
                   body={
                     <>
-                      A basic application that helps the user keep track of their financial obligations. All app data is
-                      stored locally in the browser using localstorage. This is a frontend only application, built using
-                      React in Typescript.
+                      A basic application that helps the user keep track of
+                      their financial obligations. All app data is stored
+                      locally in the browser using localstorage. This is a
+                      frontend only application, built using React in
+                      Typescript.
                       <br />
                       <br />
-                      It makes use of a number of modern web APIs that not only run the core of the application, but add
-                      additional functionality (Web Vibration API).
+                      It makes use of a number of modern web APIs that not only
+                      run the core of the application, but add additional
+                      functionality (Web Vibration API).
                     </>
                   }
                 />
@@ -324,55 +328,21 @@ export function App() {
             />
 
             <Item
-              title="Plant Lapse"
-              description="A plant management script designed to run on a Raspberry Pi"
-              body={
-                <ItemLayout
-                  title="Plant Lapse"
-                  body={
-                    <>
-                      A plant management script designed to run on a Raspberry Pi, written in Python.
-                      <br />
-                      <br />
-                      The ultimate goal for this project is to create an application that runs on a Raspberry Pi that
-                      makes use of external sensors to monitor the health of plants. The application will be able to
-                      send alerts to the user when a plant is in need of care with historical data being stored,
-                      creating a timeline of the plant's life.
-                      <br />
-                      <br />
-                      This application is in the early stages of development, currently being designed for the Raspberry
-                      Pi 5 as my main development board.
-                    </>
-                  }
-                />
-              }
-              links={[
-                {
-                  label: "Project source on GitHub",
-                  href: "https://github.com/acol248/plant-lapse",
-                  icon: (
-                    <svg viewBox="0 0 24 24">
-                      <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path>
-                    </svg>
-                  ),
-                },
-              ]}
-            />
-
-            <Item
               title="Focus"
               description="A basic experiment that makes use of the WakeLock API, now available in all mainstream browsers"
               body={
                 <ItemLayout
                   title="Focus Web App"
+                  date="2023"
                   body={
                     <>
-                      A basic experiment that makes use of the WakeLock API, now available in all mainstream browsers.
-                      This is a frontend only application, built using React in Typescript.
+                      A basic experiment that makes use of the WakeLock API, now
+                      available in all mainstream browsers. This is a frontend
+                      only application, built using React in Typescript.
                       <br />
                       <br />
-                      The goal is to encourage a user to put down their phone and focus on doing something in the real
-                      world
+                      The goal is to encourage a user to put down their phone
+                      and focus on doing something in the real world
                     </>
                   }
                 />
