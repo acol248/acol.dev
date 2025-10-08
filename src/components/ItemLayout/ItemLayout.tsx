fb4ff91bf00de82fe0e23a33484da43e9ba93bba
acol248 <alex.collyer@acol.dev>
2025-10-08 21:18:31 +0100

    Add date to ItemLayout component

--- src/components/ItemLayout/ItemLayout.tsx
+++ src/components/ItemLayout/ItemLayout.tsx
@@ -5,6 +5,7 @@ import maps from "./ItemLayout.module.scss";
 // types
 export interface ItemLayoutProps {
   title: string;
+  date: string;
   body: React.ReactNode;
   images?: Array<{
     src: string;
@@ -14,12 +15,23 @@ export interface ItemLayoutProps {
 
 const mc = mapClassesCurried(maps, true);
 
-export default function ItemLayout({ title, body, images }: ItemLayoutProps) {
-  const classList = useClassList({ defaultClass: "item-layout", maps, string: true });
+export default function ItemLayout({
+  title,
+  date,
+  body,
+  images,
+}: ItemLayoutProps) {
+  const classList = useClassList({
+    defaultClass: "item-layout",
+    maps,
+    string: true,
+  });
 
   return (
     <div className={classList}>
       <p className={mc("item-layout__title")}>{title}</p>
+      <span className={mc("item-layout__date")}>{date}</span>
+
       <pre className={mc("item-layout__description")}>{body}</pre>
 
       {images && images.length > 0 && (
